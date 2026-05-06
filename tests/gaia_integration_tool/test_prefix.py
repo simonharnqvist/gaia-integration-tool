a0d7bae7f07f331925acb5a03fca42d4371d3f85
simonharnqvist <seharnqvist@gmail.com>
2026-05-06 09:10:34 +0000

    simplified layout

--- tests/gaia_integration_tool/test_prefix.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import Row
-from gaia_integration_tool.join.prefix import prefix_columns
-
-def test_prefix_columns(spark):
-    df = spark.createDataFrame([Row(id = 0, a=1, b=2)])
-    out = prefix_columns(df, "p", exclude=["id"])
-    assert set(out.columns) == {"id", "p_a", "p_b"}
